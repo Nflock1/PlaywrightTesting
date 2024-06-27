076b6e57f2d2d7c97314e7eb2b34107e4b887b1d
Nick <nbw.kcin@gmail.com>
2024-06-27 17:25:31 -0500

    no need for special formatting on spaces

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -56,7 +56,7 @@ test.describe('Asana Data-Driven Tests', () => {
 
         await page.locator(`input[type="password"]`).fill("Password123");
         await page.getByRole('button', {name: "Log in", exact:true}).click()
-              });
+      });
 
       await test.step('Navigate to the project page', async () => {
         // Navigate to the project page
@@ -65,7 +65,7 @@ test.describe('Asana Data-Driven Tests', () => {
 
       await test.step('Verify the card is within the right column', async () => {
         // Verify the card is within the right column
-        const columnHeaderAncestor = await page.locator(`.BoardBody-columnDraggableItemWrapper:has-text('${data.column.replace("\\s", "&nbsp")}')`)
+        const columnHeaderAncestor = await page.locator(`.BoardBody-columnDraggableItemWrapper:has-text('${data.column}')`)
         const card = await columnHeaderAncestor.locator(`.BoardCardLayout-title:has-text('${data.card_title}')`,)
         await expect(card).toHaveCount(1, {timeout: 10000})
       });
